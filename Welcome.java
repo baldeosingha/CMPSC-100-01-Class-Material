06a0d4742ca1dea482f9853daf529b4f781dbabd
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-09-05 14:18:48 -0400

    modified program from sept 3-5 classes.

--- Welcome.java
+++ Welcome.java
@@ -1,8 +1,14 @@
 /** Hello World program in Java.
+* This program shows examples of String
+* concatenation and escape sequence usage.
 */
 
-public class Welcome {
+public     class    Welcome {
   public static void main (String [] args) {
     System.out.println("Hello World!");
+    System.out.print("Hi\t");
+    System.out.print("there! \n");
+    System.out.println("Adding " + 12 + " "+ 23);
+    System.out.println("Adding "+ (12 + 23));
   }
 }
