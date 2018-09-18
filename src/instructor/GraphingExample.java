46c0d7172159e998304ce30d4e7de2a7248344fc
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-09-18 16:19:50 -0400

    Added an empty program template for sept 19

---
+++ src/instructor/GraphingExample.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/** Program to perform basic graphing functions
+@author Janyl Jumadinova
+*/
+public class GraphingExample extends Application {
+    /** main method to create and display
+    * various shapes.
+    */
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
--- src/instructor/HelloJavaFX.java
+++ src/instructor/HelloJavaFX.java
@@ -1,6 +1,6 @@
 //************************************************************************
 //  HelloJavaFX.java       Author: Lewis/Loftus
-//
+// 
 //  Demonstrates a basic JavaFX application.
 //************************************************************************
 
@@ -11,30 +11,27 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class HelloJavaFX extends Application
-{
+public class HelloJavaFX extends Application {
     //--------------------------------------------------------------------
     //  Creates and displays two Text objects in a JavaFX window.
     //--------------------------------------------------------------------
-    public void start(Stage primaryStage)
-    {
+    public void start(Stage primaryStage) {
         Text hello = new Text(50, 50, "Hello, JavaFX!");
         Text question = new Text(120, 80, "How's it going?");
-        
+
         Group root = new Group(hello, question);
         Scene scene = new Scene(root, 300, 120, Color.LIGHTGREEN);
-        
+
         primaryStage.setTitle("A JavaFX Program");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
+
     //--------------------------------------------------------------------
     //  Launches the JavaFX application. This method is not required
     //  in IDEs that launch JavaFX applications automatically.
     //--------------------------------------------------------------------
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 }
